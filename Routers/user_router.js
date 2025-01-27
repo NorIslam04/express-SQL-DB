b251deaf62c642cffd608e2002c180eba1e5f98c
NorIslam04 <islamaoudi18@gmail.com>
2025-01-27 16:10:11 +0100

    add index page

--- Routers/user_router.js
+++ Routers/user_router.js
@@ -5,8 +5,12 @@ const user_controller=require('../Controllers/user_controller');
 
 const router=express.Router();
 
-router.get('',user_controller.getUsers);
+router.get('/getUser',user_controller.getUsers);
 router.post('/creatUser',user_controller.createUser);
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
 
 
 module.exports=router;
